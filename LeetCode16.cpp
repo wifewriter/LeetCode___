1f585d50437c3ebd8870eaac25e21364cd295bfb
yangyang <912962140@qq.com>
2022-04-21 13:24:04 +0800

    11

--- LeetCode16.cpp
+++ LeetCode16.cpp
@@ -35,49 +35,99 @@
 #include <algorithm>
 using namespace std;
 
+/**
+ * 这个方法只能过90%
+ */
+//class Solution {
+//public:
+//    int threeSumClosest(vector<int>& nums, int target) {
+//        sort(nums.begin(),nums.end(),less<int>());
+//        auto result = pow(10,4)+1;
+//        for (int i = 0; i < nums.size() - 2; ++i) {
+//            int j = i + 1;
+//            bool flag = false;
+//            for (; j < nums.size() - 1; ++j) {
+//                if (j == i + 1) {
+//                    auto k = nums.size() - 1;
+//                    while (k > j) {
+//                        if (abs(nums[i] + nums[j] + nums[k] - target) < abs(result - target)) {
+//                            result = nums[i] + nums[j] + nums[k];
+//                            k--;
+//                        } else break;
+//                    }
+//                } else {
+//                    auto k = j + 1;
+//                    while (k < nums.size()) {
+//                        if (!flag and abs(nums[i] + nums[j] + nums[k] - target) > abs(result - target)) {
+//                            k--;
+//                        } else {
+//                            if (!flag) {
+//                                flag = true;
+//                                result = nums[i] + nums[j] + nums[k];
+//                            } else {
+//                                break;
+//                            }
+//                        }
+//                    }
+//                }
+//                if (flag)break;
+//            }
+//        }
+//        return result;
+//    }
+//};
+
+/**
+ * 直接简单穷举+条件退出。
+ */
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end(),less<int>());
-        int index_0 = 1001;
-        int result = pow(10,4)+1;
-        for (int i = 0; i <nums.size()-2 ; ++i) {
-            if(i == 0 or nums[i] != index_0){
-                index_0 = nums[i];
-                int j = i + 1;
-                bool flag = false;
-                for (; j < nums.size() - 1; ++j) {
-                    if (j == i + 1) {
-                        auto k = nums.size() - 1;
-                        while (k > j) {
-                            if (abs(nums[i] + nums[j] + nums[k] - target) < abs(result - target)) {
-                                result = nums[i] + nums[j] + nums[k];
-                                k--;
-                            } else break;
-                        }
-                    } else{
-                        auto k = j +1;
-                        while (k < nums.size()) {
-                            if (!flag and abs(nums[i] + nums[j] + nums[k] - target) > abs(result - target)) {
-                                k--;
-                            } else{
-                                if (!flag) {
-                                    flag = true;
-                                    result = nums[i] + nums[j] + nums[k];
-                                } else{
-                                    break;
+    int threeSumClosest(vector<int> &nums, int target) {
+        sort(nums.begin(), nums.end(), less<int>());
+        auto result = pow(10, 4) + 1;
+        int index0{};
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            if (i == 0 or nums[i] != index0) {
+                index0 = nums[i];
+                int index1{};
+                for (int j = i + 1; j < nums.size() - 1; ++j) {
+                    if (j == i + 1 or nums[j] != index1) {
+                        index1 = nums[j];
+                        auto k = nums.size()-1;
+                        while (j < k){
+                            auto resultTemp = nums[i] + nums[j] + nums[k];
+                            if(resultTemp > target) {
+                                //参考的官方去重代码
+                                auto k0 = k - 1;
+                                // 移动到下一个不相等的元素
+                                while (j < k0 && nums[k0] == nums[k]) {
+                                    --k0;
                                 }
+                                k = k0;
+                            }
+                            else if(resultTemp <target) {
+                                // 参考的官方去重代码
+                                int j0 = j + 1;
+                                // 移动到下一个不相等的元素
+                                while (j0 < k && nums[j0] == nums[j]) {
+                                    ++j0;
+                                }
+                                j = j0;
+                            }
+                            else{
+                                return target;
+                            }
+                            if (abs(result - target) > abs(resultTemp - target)) {
+                                result = resultTemp;
                             }
                         }
                     }
-                    if(flag)break;
                 }
             }
         }
         return result;
     }
 };
-
 int main(){
     vector<int> a = {1,2,4,8,16,32,64,128};
     int target = 82;
