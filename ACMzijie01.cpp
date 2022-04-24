2a301efc08ff77f5219401eab7b8104f1d3b5b8d
912962140@qq.com <Yangyang1230.0>
2022-04-24 13:40:15 +0800

    04 24

--- ACMzijie01.cpp
+++ ACMzijie01.cpp
@@ -1,11 +1,11 @@
 /**
  * 题目:
 4
-7
-1 1 0 1 0 1 0
-1 0 1 0 1 0 1
-0 0 0 0 0 0 0
-1 1 1 1 0 0 0
+8
+1 1 0 1 0 1 0 1
+1 0 1 0 1 0 1 0
+0 0 0 0 0 0 0 1
+1 1 1 1 0 0 0 0
  *  输出相邻0的最大个数。
  */
 #include <iostream>
@@ -30,113 +30,87 @@ void findXY(vector<vector<char>> &game, int &x, int &y) {
     y = game[0].size();
 }
 
+void changeStatus(vector<vector<char>> &game,queue<tuple<int, int>> &road,int indexX, int indexY, int &result){
+    road.push({indexX,indexY});
+    game[indexX][indexY] = '1';
+    result++;
+}
+
 //广度
 void BFS(vector<vector<char>> &game, queue<tuple<int, int>> &road, int &indexX, int &indexY, int &result) {
     game[indexX][indexY] = '1';
     if (indexX == 0) {
         //右边
         if (indexY + 1 < game[0].size() and game[indexX][indexY + 1] == '0') {
-            road.push(tuple<int, int>(indexX, indexY + 1));
-            game[indexX][indexY + 1] = '1';
-            result++;
+            changeStatus(game,road,indexX,indexY+1,result);
         }
         //左边
         if (indexY - 1 >= 0 and game[indexX][indexY - 1] == '0') {
-            road.push(tuple<int, int>(indexX, indexY - 1));
-            game[indexX][indexY - 1] = '1';
-            result++;
+            changeStatus(game,road,indexX,indexY-1,result);
         }
         //下边
         if (indexX + 1 < game.size() and game[indexX + 1][indexY] == '0') {
-            road.push(tuple<int, int>(indexX + 1, indexY));
-            game[indexX + 1][indexY] = '1';
-            result++;
+            changeStatus(game,road,indexX+1,indexY,result);
         }
     } else if (indexX == game.size() - 1) {
         //右边
         if (indexY + 1 < game[0].size() and game[indexX][indexY + 1] == '0') {
-            road.push(tuple<int, int>(indexX, indexY + 1));
-            game[indexX][indexY + 1] = '1';
-            result++;
+            changeStatus(game,road,indexX,indexY+1,result);
         }
         //左
         if (indexY - 1 >= 0 and game[indexX][indexY - 1] == '0') {
-            road.push(tuple<int, int>(indexX, indexY - 1));
-            game[indexX][indexY - 1] = '1';
-            result++;
+            changeStatus(game,road,indexX,indexY-1,result);
         }
         //上
         if (indexX - 1 >= 0 and game[indexX - 1][indexY] == '0') {
-            road.push(tuple<int, int>(indexX, indexY + 1));
-            game[indexX - 1][indexY] = '1';
-            result++;
+            changeStatus(game,road,indexX-1,indexY,result);
         }
     } else if (indexY == game[0].size() - 1) {
 
         //下
         if (indexX + 1 < game.size() and game[indexX + 1][indexY] == '0') {
-            road.push(tuple<int, int>(indexX + 1, indexY));
-            game[indexX + 1][indexY] = '1';
-            result++;
+            changeStatus(game,road,indexX+1,indexY,result);
         }
         //上
         if (indexX - 1 >= 0 and game[indexX - 1][indexY] == '0') {
-            road.push(tuple<int, int>(indexX, indexY + 1));
-            game[indexX - 1][indexY] = '1';
-            result++;
+            changeStatus(game,road,indexX-1,indexY,result);
         }
         //左边
         if (indexY - 1 >= 0 and game[indexX][indexY - 1] == '0') {
-            road.push(tuple<int, int>(indexX, indexY - 1));
-            game[indexX][indexY - 1] = '1';
-            result++;
+            changeStatus(game,road,indexX,indexY-1,result);
         }
     } else if (indexY == 0) {
         //下边
-        if (indexX < game.size() - 1 and game[indexX + 1][indexY] == '0') {
-            road.push(tuple<int, int>(indexX + 1, indexY));
-            game[indexX + 1][indexY] = '1';
-            result++;
+        if (indexX+1 < game.size() and game[indexX + 1][indexY] == '0') {
+            changeStatus(game,road,indexX+1,indexY,result);
         }
         //上
-        if (indexX > 0 and game[indexX - 1][indexY] == '0') {
-            road.push(tuple<int, int>(indexX, indexY + 1));
-            game[indexX - 1][indexY] = '1';
-            result++;
+        if (indexX-1 >= 0 and game[indexX - 1][indexY] == '0') {
+            changeStatus(game,road,indexX-1,indexY,result);
         }
 
         //右
         if (indexY + 1 < game[0].size() and game[indexX][indexY + 1] == '0') {
-            road.push(tuple<int, int>(indexX, indexY + 1));
-            game[indexX][indexY + 1] = '1';
-            result++;
+            changeStatus(game,road,indexX,indexY+1,result);
         }
 
     }
     if (indexX > 0 and indexX < game.size() - 1 and indexY > 0 and indexY < game[0].size() - 1) {
         //左
         if (game[indexX][indexY - 1] == '0') {
-            road.push(tuple<int, int>(indexX, indexY - 1));
-            game[indexX][indexY - 1] = '1';
-            result++;
+            changeStatus(game,road,indexX,indexY-1,result);
         }
         //右
         if (game[indexX][indexY + 1] == '0') {
-            road.push(tuple<int, int>(indexX, indexY + 1));
-            game[indexX][indexY + 1] = '1';
-            result++;
+            changeStatus(game,road,indexX,indexY+1,result);
         }
         //下
         if (game[indexX + 1][indexY] == '0') {
-            road.push(tuple<int, int>(indexX + 1, indexY));
-            game[indexX + 1][indexY] = '1';
-            result++;
+            changeStatus(game,road,indexX+1,indexY,result);
         }
         //上
         if (game[indexX - 1][indexY] == '0') {
-            road.push(tuple<int, int>(indexX, indexY + 1));
-            game[indexX - 1][indexY] = '1';
-            result++;
+            changeStatus(game,road,indexX-1,indexY,result);
         }
     }
 }
