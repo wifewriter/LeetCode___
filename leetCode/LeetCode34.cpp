0ffad3c48ec1087dd0e90c6e722d918dcf40d8f1
912962140@qq.com <Yangyang1230.0>
2022-05-08 18:01:12 +0800

    0508

---
+++ leetCode/LeetCode34.cpp
@@ -0,0 +1,22 @@
+/**
+ * 题目:
+ *
+ */
+#include <iostream>
+#include "string"
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+    }
+};
+
+int main(){
+
+    Solution sol;
+    cout<<sol
+    return 0;
+}
