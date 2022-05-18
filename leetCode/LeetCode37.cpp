33116208950be3d17917b5643e0d54b0946b1716
yangyang <912962140@qq.com>
2022-05-19 00:25:55 +0800

    0517 750m

--- leetCode/LeetCode37.cpp
+++ leetCode/LeetCode37.cpp
@@ -24,7 +24,7 @@ private:
         return {-1,-1};
     }
     vector<char> findInsertNums(vector<vector<char>> & board,int &x,int &y){
-        vector<int> T(10,0);
+        int T[10] = {0};
         T[0] = true;
         for (int i = 0; i < board.size() ; ++i) {
             T[board[x][i] -'0'] = 1;
@@ -40,7 +40,7 @@ private:
         vector<char> result{};
         for (int k = 1; k < 10 ; ++k) {
             if(!T[k]){
-                result.emplace_back(k+'0');
+                result.push_back(k+'0');
             }
         }
         return result;
@@ -52,7 +52,7 @@ private:
         } else{
             int x = get<0>(result);
             int y = get<1>(result);
-            vector<char> insertT = findInsertNums(board,x,y);
+            auto insertT = findInsertNums(board,x,y);
             if(insertT.empty()){
                 return false;
             }
@@ -68,9 +68,9 @@ public:
 };
 
 int main(){
-    vector<vector<char>> board = {{"5","3",".",".","7",".",".",".","."},{"6",".",".","1","9","5",".",".","."},{".","9","8",".",".",".",".","6","."},{"8",".",".",".","6",".",".",".","3"},{"4",".",".","8",".","3",".",".","1"},{"7",".",".",".","2",".",".",".","6"},{".","6",".",".",".",".","2","8","."},{".",".",".","4","1","9",".",".","5"},{".",".",".",".","8",".",".","7","9"}};
+    vector<vector<char>> board{{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
 
     Solution sol;
-    cout<<sol
+    sol.solveSudoku(board);
     return 0;
 }
